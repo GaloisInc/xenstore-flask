f7fef510d7315817edc3d5b3c19fb60b0c449875
James Bielman <jamesjb@galois.com>
2013-08-08 16:06:42 -0700

    Use the xenstore unlabeled initial SID when errors occur.

--- src/hooks.ml
+++ src/hooks.ml
@@ -24,6 +24,14 @@ let rec last xs =
   | x :: [] -> x
   | x :: xs -> last xs
 
+(* Wrapper around "context_to_sid" that returns the predefined
+ * unlabeled SID if an error occurs. *)
+let safe_context_to_sid label =
+  try
+    Sec.context_to_sid label
+  with Failure _ ->
+    Policy.InitialSID.xenstore_unlabeled
+
 (* Label a newly created node based on the path database
  * and label of the (already existing) parent node. *)
 let new_node_label path_db path parent_label =
@@ -39,15 +47,15 @@ let new_node_label path_db path parent_label =
         raise (Failure "new_node_label: result of Value_none")
       (* transition(parent_label, path_db_label) -> new_label *)
       | Path_db.Value_str label ->
-        let sid1 = Sec.context_to_sid parent_label in
-        let sid2 = Sec.context_to_sid label in
+        let sid1 = safe_context_to_sid parent_label in
+        let sid2 = safe_context_to_sid label in
         let sid3 = Sec.create sid1 sid2 Policy.Class.xenstore in
         Sec.sid_to_context sid3
       (* transition(getdomainsid(last(wilds)), parent_label) -> new_label *)
       | Path_db.Value_domid ->
         let domid = int_of_string (last (Path_db.(r.result_wilds))) in
         let sid1  = OS.Flask.getdomainsid domid in
-        let sid2  = Sec.context_to_sid parent_label in
+        let sid2  = safe_context_to_sid parent_label in
         let sid3  = Sec.create sid1 sid2 Policy.Class.xenstore in
         Sec.sid_to_context sid3)
 
@@ -66,7 +74,7 @@ let node_access_audit_data domid path =
 let node_access dom_id node_path node_label av =
   let ad = node_access_audit_data dom_id node_path in
   let dom_sid = OS.Flask.getdomainsid dom_id in
-  let node_sid = Sec.context_to_sid node_label in
+  let node_sid = safe_context_to_sid node_label in
   if not (Sec.has_perm itf dom_sid node_sid Class.xenstore av ad)
     then raise Xenstore_server.Perms.Permission_denied
     else ()
